46a2b7c606fe7beb0c2ed48eb48811a9ed9e8431
JamesLiu12 <39219648+JamesLiu12@users.noreply.github.com>
2023-10-18 14:42:03 +0800

    delete outline input

--- Generator.py
+++ Generator.py
@@ -1,11 +1,18 @@
 import g4f
 import json
+import openai
+
+openai.api_base = "https://api.aiohub.org/v1"
+openai.api_key = 'e8d20ad67ba241228469ae8c37877f41'
 
 
 class Generator:
-    def __init__(self, provider=g4f.Provider.Aichat, model="gpt-3.5-turbo"):
+    # def __init__(self, provider=g4f.Provider.Aichat, model="gpt-3.5-turbo"):
+    #     self.model = model
+    #     self.provider = provider
+
+    def __init__(self, model="gpt-3.5-turbo"):
         self.model = model
-        self.provider = provider
 
     @staticmethod
     def __read_instruction(task_type):
@@ -13,28 +20,34 @@ class Generator:
             return json.load(file)[task_type]
 
     def __get_completion_from_messages(self, messages):
-        response = g4f.ChatCompletion.create(
-            provider=self.provider,
+        # response = g4f.ChatCompletion.create(
+        response = openai.ChatCompletion.create(
+            # provider=self.provider,
             model=self.model,
             messages=messages,
             temperature=0.1,  # this is the degree of randomness of the model's output
+            request_timeout=100,
         )
-        return response
+        return response.choices[0]['message']['content']
 
-    def __get_response_from_list(self, instruction, outline):
+    def __get_response_from_list(self, instruction, text_input):
         messages = [
             {"role": "system", "content": instruction},
+            {"role": "user", "content": text_input}
         ]
-        for text in outline:
-            messages.append({"role": "user", "content": text})
+        print(instruction)
+        print(text_input)
+        # for text in text_input:
+        #     messages.append({"role": "user", "content": text})
         return self.__get_completion_from_messages(messages)
 
     def generate_outline(self, teaching_material: list):
         instruction = self.__read_instruction("generate outline")
-        list_concat = ["Here is the input text:"] + teaching_material
-        return self.__get_response_from_list(instruction, teaching_material)
+        text_concat = "Here is the input text:" + "\n" + "".join(teaching_material)
+        return self.__get_response_from_list(instruction, text_concat)
 
     def generate_content(self, teaching_material: list, outline: list):
         instruction = self.__read_instruction("generate content")
-        list_concat = ["Here is the input text:"] + teaching_material + ["Here is the outline:"] + outline
-        return self.__get_response_from_list(instruction, list_concat)
+        # text_concat = "Here is the outline:\n" + "".join(outline) + "Please specify each step in the outline."
+        text_concat = "Here is the input text:\n" + "".join(teaching_material)
+        return self.__get_response_from_list(instruction, text_concat)
